2ec5f2006e44143ee7db745793db8ec791a1f84a
YouT <newfish@fishxiaoutekiMacBook-Pro.local>
2020-07-16 11:25:18 +0800

    hhh

---
+++ testProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testProject
+//
+//  Created by YouT on 2020/7/16.
+//  Copyright © 2020 fsToone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testProject/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  testProject
+//
+//  Created by YouT on 2020/7/16.
+//  Copyright © 2020 fsToone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
